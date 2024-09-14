97086c0411ffe87a21469426e2f42124c31a062c
John Leahy <jleahy@immediac.com>
2024-09-14 07:42:12 -0300

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by John Leahy on 2024-09-14.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FitnessAppTests/FitnessAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppTests.swift
+//  FitnessAppTests
+//
+//  Created by John Leahy on 2024-09-14.
+//
+
+import Testing
+@testable import FitnessApp
+
+struct FitnessAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
